69bb096f1f2e46a3c09ddc90fdd8ddb092fa7bca
nathaliacosim <nathalia.cosim@novaandradina.org>
2019-09-30 17:07:46 -0400

    novo

--- src/Valida/index.js
+++ src/Valida/index.js
@@ -1,52 +1,165 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container } from './styles';
 
-export default function Valida() {
-  const [ip, setIp] = useState('');
-  let val = useState('');
-  let classe = useState('gi');
+const initialState = {
+  ip: "",
+  valido: "",
+  classe: "",
+  mascara: "",
+  funcMascara: "",
+  endRede: "",
+  endIniRede: "",
+  endFimRede: "",
+  endBroad: "",
+  resLAN: "",
+  ipBin: ""
+};
 
-  function handleSubmit(e){
-    e.preventDefault();
-    console.log(ip);
-  }
- 
-  function valida(){
-    const octetos = ip.split(".");
-    if(octetos.length <= 3) console.log('Inválido');
-    else {
-      const oct1 = octetos[0];
-      if(oct1 > 0 && oct1 <= 126) {
-        val = "Valido" 
-        classe = "A"
-      } 
-      else if(oct1 >= 128 && oct1 <= 191) console.log("Classe B")
-      else if(oct1 >= 192 && oct1 <= 223) console.log("Classe C")
-      else if(oct1 >= 224 && oct1 <= 239) console.log("Classe D")
-      else if(oct1 >= 240 && oct1 <= 254) console.log("Classe E")
-      else console.log("Inválido!")
+export default class Valida extends React.Component {
+  state = initialState;
+
+  handleChange = event => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value
+    });
+  };
+
+  validate = () => {
+    let ips = this.state.ip;
+    let octetos = ips.split(".")
+    let valido = "";
+    let classe = "";
+    let mascara = "";
+    let endRede = "";
+    let endIniRede = "";
+    let endFimRede= "";
+    let endBroad = "";
+    let resLAN = "";
+
+    let oct1 = octetos[0];
+    let oct2 = octetos[1];
+    let oct3 = octetos[2];
+    let oct4 = octetos[3];
+
+    if (octetos.length <= 3 || oct1 > 255 || oct2 > 255 || oct3 > 255 || oct4 > 255) { 
+      classe = "IP Inválido!";
+      valido = "IP Inválido!";
+      mascara = "IP Inválido!";
+      endRede = "IP Inválido!";
+      endIniRede = "IP Inválido!";
+      endFimRede = "IP Inválido!";
+      endBroad = "IP Inválido!";
+      resLAN = "IP Inválido!";
+    } else {
+      if (oct1 > 0 && oct1 <= 126) {
+        classe = "Classe A";
+        valido = "IP Válido!";
+        mascara = "255.0.0.0";
+        endRede = oct1 + ".0.0.0";
+        endIniRede = oct1 + ".0.0.1";
+        endFimRede = oct1 + ".255.255.254";
+        endBroad = oct1 + ".255.255.255";
+        resLAN = "10.0.0.0";
+      } else if (oct1 >= 128 && oct1 <= 191) {
+        classe = "Classe B";
+        valido = "IP Válido!";
+        mascara = "255.255.0.0";
+        endRede = oct1 + "." + oct2 + ".0.0";
+        endIniRede = oct1 + "." + oct2 + ".0.1";
+        endFimRede = oct1 + "." + oct2 + ".255.254";
+        endBroad = oct1 + "." + oct2 + ".255.255";
+        resLAN = "172.16.0.1";
+      } else if(oct1 >= 192 && oct1 <= 223) {
+        classe = "Classe C";
+        valido = "IP Válido!";
+        mascara = "255.255.255.0";
+        endRede = oct1 + "." + oct2 + "." + oct3 + ".0";
+        endIniRede = oct1 + "." + oct2 + "." + oct3 + ".1";
+        endFimRede = oct1 + "." + oct2 + "." + oct3 + ".254";
+        endBroad = oct1 + "." + oct2 + "." + oct3 + ".255";
+        resLAN = "192.168.0.0";
+      } else if(oct1 >= 224 && oct1 <= 239) {
+        classe = "Classe D";
+        valido = "IP Válido!";
+        mascara = "Utilizado para tráfego Multicast";
+        endRede = " - - - - ";
+        endIniRede = " - - - - ";
+        endFimRede = " - - - - ";
+        endBroad = " - - - - ";
+        resLAN = " - - - - ";
+      } else if(oct1 >= 240 && oct1 <= 254) {
+        classe = "Classe E";
+        valido = "IP Válido!";
+        mascara = "Reservado para uso futuro e testes"
+        endRede = " - - - - ";
+        endIniRede = " - - - - ";
+        endFimRede = " - - - - ";
+        endBroad = " - - - - ";
+        resLAN = " - - - - ";
+      } else {
+        classe = "IP Inválido!";
+        valido = "IP Inválido!";
+        mascara = "IP Inválido!";
+        endRede = "IP Inválido!";
+        endIniRede = "IP Inválido!";
+        endFimRede = "IP Inválido!";
+        endBroad = "IP Inválido!";
+        resLAN = "IP Inválido!";
+      }
     }
+
+    if(classe || valido || mascara || endRede || endIniRede || endFimRede || endBroad || resLAN){
+      this.setState({classe, valido, mascara, endRede, endIniRede, endFimRede, endBroad, resLAN})
+      return false
+    }
+
+    return true;
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+      console.log(this.state);
+      // clear form
+      this.setState(initialState);
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <div className="login-container">
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label htmlFor="ip">Insira o IP: </label>
+              <input
+                id="ip"
+                name="ip"
+                placeholder="(Ex: 192.168.254.6)"
+                value={this.state.ip}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button type="submit">Validar</button>
+          </form>
+
+          <div className="result">
+            <strong> IP: {this.state.ip} </strong>
+            <strong> É válido?  {this.state.valido} </strong>
+            <strong> Classe: {this.state.classe} </strong>  
+            <strong> Mascara: {this.state.mascara} </strong>  
+            <strong> Endereço de rede: {this.state.endRede} </strong>
+            <strong> Endereço Inicial utilizável da Rede: {this.state.endIniRede} </strong>
+            <strong> Endereço Final utilizável da Rede: {this.state.endFimRede} </strong>
+            <strong> Endereço Broadcast: {this.state.endBroad} </strong>
+            <strong> Endereço LAN reservado: {this.state.resLAN} </strong>
+          </div>
+        </div>
+      </Container>
+    );
   }
-
-
-
-  return (
-    <Container>
-      <div className="login-container">
-        <form onSubmit={handleSubmit}>
-          <input 
-            placeholder="Insira o IP (Ex: 192.168.254.6)"
-            value={ip}
-            onChange={e => setIp(e.target.value)}
-          />
-          <button type="submit" onClick={valida}>Validar</button>
-         </form>
-      </div>
-      <div className="login-container">
-        <h2>Status</h2>
-        <p>{val}</p>
-        <p>{classe}</p>
-      </div>
-    </Container>
-  );
 }
--- src/Valida/styles.js
+++ src/Valida/styles.js
@@ -3,35 +3,47 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: 100%;
   align-items: center;
-  background-color: #DF4723;
 
   .login-container {
-    height: 100%;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
+    background: #ff9068;
+    background: linear-gradient(45deg, #fd746c , #ff9068);
+    height: 100%;
+    padding-bottom: 30px;
   }
 
   .login-container form {
-    width: 100%;
-    max-width: 500px;
+    min-width: 100px;
+    max-width: 80%;
+    min-height: 200px;
+    max-height: 100%;
+    background: #fafafa;
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    box-shadow: 0 0 5px rgba(25,25,25,.25);
+    padding: 3rem;
   }
 
   .login-container form input{
-    margin-top: 20px;
-    border: 1px solid #ddd;
-    border-radius: 4px;
-    height: 48px;
-    padding:  0 20px;
-    font-size: 16px;
-    color: #666;
+    width: 100%;
+		border-radius: 5px;
+		padding: 1rem 4rem;
+		background: #e8ebed;
+    margin: 0;
+    outline: 0;
+    border: 2px solid #e8ebed;
   }
 
   .login-container form input::placeholder{
     color: #999;
     text-align: center;
+    font-size: 14px;
   }
 
   .login-container form button {
@@ -39,13 +51,31 @@ export const Container = styled.div`
     border: 0;
     border-radius: 4px;
     height: 48px;
+    width:100%;
     font-size: 16px;
-    background: #999;
+    background: #DF4723;
     color: #fff;
     font-weight: bold;
     cursor: pointer;
   }
   p {
     color: #fff;
+  }
+
+  .result {
+    margin-top: 10%;
+    font-size: 18px;
+    font-family: Roboto;
+    display: flex;
+    justify-content: center;
+    align-items: left;
+    flex-direction: column;
+  }
+
+  label {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 20px;
+    font-weight: 900;
+    color: #1E1F26;
   }
 `;
