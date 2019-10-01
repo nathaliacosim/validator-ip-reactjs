aa5f94a0e4897b8d14e7d56476ad09cff06219c2
Nathalia Cosim <nathalia.cosim@novaandradina.org>
2019-10-01 11:13:32 -0400

    Update global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,27 +2,29 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500&display=swap');
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
   html, body, #root {
     height: 100%;
   }
+
   body {
     font: 14px 'Roboto', sans-serif;
     background: #1c1c1c;
     -webkit-font-smoothing: antialiased !important;
   }
+
   ul {
     list-style: none;
   }
+
   a {
     color: #fff;
     text-decoration: none;
-  }
-  .main-container {
-    margin-top: 100px;
   }
 `;
